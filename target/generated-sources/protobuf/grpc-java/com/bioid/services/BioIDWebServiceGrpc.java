5d8d17bab8f6ce98214888d4707fe728ca814161
alexb-bioid <a.bernst@bioid.de>
2025-01-07 12:01:38 +0100

    Update of packages for gRPC (liveness detection, deepfake detection etc.) and spring boot

--- target/generated-sources/protobuf/grpc-java/com/bioid/services/BioIDWebServiceGrpc.java
+++ target/generated-sources/protobuf/grpc-java/com/bioid/services/BioIDWebServiceGrpc.java
@@ -8,7 +8,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.64.0)",
+    value = "by gRPC proto compiler (version 1.69.0)",
     comments = "Source: bws.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class BioIDWebServiceGrpc {
